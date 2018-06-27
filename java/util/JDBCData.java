be78b6a6b20f4f1b5f1df7d0c22da60ae77bbeed
hebu999 <40182180+hebu999@users.noreply.github.com>
2018-06-27 15:08:05 +0200

    JPA

--- java/util/JDBCData.java
+++ java/util/JDBCData.java
@@ -8,26 +8,41 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.TimeZone;
+import java.util.logging.Level;
+import java.util.logging.*;
 import javax.faces.bean.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
+import javax.persistence.Persistence;
 import javax.persistence.PersistenceUnit;
+import javax.persistence.Query;
 import com.mysql.jdbc.Driver;
 import java.io.OutputStream;
 import model.Produkt;
 import model.Account;
 
 /**
- * * *
  * 
  * @author Heiner, Steffen
- *
+ * @version 1.0
+ * 
+ * @category Diese Klasse stellt eine JDBC Verbindung zur Datenbank her und speichert die
+ *           Datensätzen in zwei ArrayLists.
+ * 
  */
 
-@Named(value = "JDBC")
+@Named(value = "JDBCData")
 @ApplicationScoped
 public class JDBCData implements Serializable {
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+  private final Logger logger = Logger.getLogger(this.getClass().getName());
+
   List<Produkt> productList = new ArrayList<Produkt>();
   List<Account> accountList = new ArrayList<Account>();
   Connection conn = null;
@@ -37,17 +52,16 @@ public class JDBCData implements Serializable {
   }
 
   @PersistenceUnit
-  private EntityManagerFactory emf;
+  private EntityManagerFactory emf = Persistence.createEntityManagerFactory("shop");
 
+  // JDBC Verbindung wird hergestellt und in die Variable "conn" geschrieben und returned
   public Connection connectDatabase() throws SQLException {
 
-
     try {
-      TimeZone timezone = TimeZone.getTimeZone("Europe/Berlin");
-      TimeZone.setDefault(timezone);
+      
       Class.forName("com.mysql.cj.jdbc.Driver");
       String dbURL =
-          "jdbc:mysql://localhost:3306/smartphoneshop2?useLegacyDatetimeCode=false&useJDBCCompliantTimezoneShift=true";
+          "jdbc:mysql://localhost:3306/smartphoneshop2?useLegacyDatetimeCode=false&useJDBCCompliantTimezoneShift=true&serverTimezone=UTC";
       conn = DriverManager.getConnection(dbURL, "root", "");
 
     } catch (ClassNotFoundException e) {
@@ -60,70 +74,72 @@ public class JDBCData implements Serializable {
     return conn;
   }
 
+  // Daten aus der Tabelle "produkt" werden abgerufen und in die productList geschrieben
   public List<Produkt> getSQLDataProduct() throws SQLException {
+    logger.setLevel(Level.ALL);
 
-    try {
+    Connection conn = connectDatabase();
+    ResultSet product = conn.createStatement().executeQuery("SELECT * FROM produkt");
 
-      Connection conn = connectDatabase();
+    while (product.next()) {
 
-      ResultSet product = conn.createStatement().executeQuery("SELECT * FROM 'produkt'");
-
-      while (product.next()) {
-
-        productList.add(new Produkt(product.getString("PName"), product.getBigDecimal("preis"),
-            product.getString("Typ"), product.getString("Kommentar")));
-      }
-      product.close();
-
-      System.out.println(productList);
-
-
+      //productList.add(new Produkt(product.getString("PName"), product.getBigDecimal("PPreisNetto"),
+        //  product.getString("PTyp"), product.getString("PKomment")));
     }
+    product.close();
 
-    catch (SQLException e) {
-      if (conn != null)
-        conn.rollback();
-      System.out.println("SQLException: " + e.getMessage());
-      System.out.println("SQLState: " + e.getSQLState());
-      System.out.println("VendorError: " + e.getErrorCode());
-
-    }
     return productList;
   }
 
+  // Daten aus der Tabelle "accounts" werden abgerufen und in die accountList geschrieben
   public List<Account> getSQLDataAccount() throws SQLException {
+    logger.setLevel(Level.ALL);
 
-    try {
-      Connection conn = connectDatabase();
+    Connection conn = connectDatabase();
+    ResultSet account = conn.createStatement().executeQuery("SELECT * FROM accounts");
 
-      ResultSet account = conn.createStatement().executeQuery("SELECT * FROM accounts");
-
-      while (account.next()) {
-
-        accountList.add(new Account(account.getString("ACCName"), account.getString("ACCPW")));
-      }
-      account.close();
-      
-    } catch (SQLException e) {
-      if (conn != null)
-        conn.rollback();
-      System.out.println("SQLException: " + e.getMessage());
-      System.out.println("SQLState: " + e.getSQLState());
-      System.out.println("VendorError: " + e.getErrorCode());
+    while (account.next()) {
+      //accountList.add(new Account(account.getString("ACCNAME"), account.getString("ACCPWD")));
     }
-
+    account.close();
     return accountList;
-
   }
-
-  public List<Produkt> getproductList() {
-
+  
+  public Account findAccountByLoginName(String accname) {
+    Account acc = null;
+    logger.setLevel(Level.ALL);
+    try {
+      EntityManager em = emf.createEntityManager();
+      Query query = em.createQuery("SELECT u from accounts u WHERE u.accname=:accname");
+      query.setParameter("accname", accname);
+      logger.info("Account query: " + query);
+      acc = (Account) query.getSingleResult();
+      em.close();
+    }
+      catch(NoResultException nre)
+      {
+          return acc;
+      }
+      catch (Exception ex)
+      {
+          throw ex;
+      }  
+    return acc;
+  }
+  
+  public List<Produkt> getProductList() {
     return productList;
   }
 
-  public List<Account> getaccountList() {
+  public void setProductList(List<Produkt> productList) {
+    this.productList = productList;
+  }
 
+  public List<Account> getAccountList() {
     return accountList;
   }
 
+  public void setAccountList(List<Account> accountList) {
+    this.accountList = accountList;
+  }
 }
